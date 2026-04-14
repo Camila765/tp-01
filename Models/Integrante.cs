424bbccb445fc1f0f1d7497e15f944b0d7396902
CAMILA BELEN STANCANELLI <49926825@ort>
2026-04-14 08:58:20 -0300

    Segundo guardado

---
+++ Models/Integrante.cs
@@ -0,0 +1,14 @@
+public class Integrante
+{
+    private int dni;
+    private string nombre;
+    private DateTime fechaNacimiento;
+    private void foto;
+    private List<string> familiares;
+    private List<string> intereses;
+
+    public void Integrante(int dni, string nombre, DateTime fechaNacimiento)
+    {
+        
+    }
+}
